d0df003f244b47fb0320f2c4cef37028d203f296
Jeff Collins <htejeff@gmail.com>
2019-08-14 18:37:26 -0700

    incremental update on last merge

--- scripts/load_background.js
+++ scripts/load_background.js
@@ -1,11 +1,20 @@
 $(function () {
      var img = new Image();
 
-     $(img).load(function () {
-          $('html').css({
-               'background': 'linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url("../Resources/background.jpg") no-repeat center center fixed;'
-          });
-     })
+     img.setAttribute('src', './Resources/background.jpg');
 
-     .attr('src', '../Resources/background.jpg');
+     img.onload = function () {
+
+          $('html')
+
+          .removeClass('loading')
+
+          .css({
+               'background': 'linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url("./Resources/background.jpg") no-repeat center center fixed;'
+          })
+
+          .addClass('loaded');
+
+     };
+
 });
