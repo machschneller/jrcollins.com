ad1ffba28448f3e2284713ab58cc2bcd2c3078ab
Jeff Collins <jeff@jrcollins.com>
2020-07-21 02:03:40 -0700

    fixed misalignments: - skills on mobile - bullet separators for navlinks
    bonus round:
    - optimized mobile layout (page width and spacing changes)

--- scripts/load_background.js
+++ scripts/load_background.js
@@ -1,7 +1,7 @@
 $(function () {
      var img = new Image();
 
-     img.setAttribute('src', './Resources/background.jpg');
+     img.setAttribute('src', './resources/background.jpg');
 
      img.onload = function () {
 
@@ -10,7 +10,7 @@ $(function () {
           .removeClass('loading')
 
           .css({
-               'background': 'linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url("./Resources/background.jpg") no-repeat center center fixed;'
+               'background': 'linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url("./resources/background.jpg") no-repeat center center fixed;'
           })
 
           .addClass('loaded');
